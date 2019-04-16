b45ef00154ecd30a11ae1279e26a8eb77649f43c
Andrew Ogden <aogden@zynga.com>
2019-04-15 17:26:46 -0700

    Hacked together mutli-bot support, background image write mode and color grouping for command efficiency

--- bot.ts
+++ bot.ts
@@ -7,8 +7,10 @@ export const COMMAND_SOURCE = 'commands.txt';
 
 class Worker {
 	private bot;
+	public name:string;
 	public async start(commandQueue:string[], bot) {
 		this.bot = bot;
+		console.log(`${this.bot.username} start`)
 		return new Promise((resolve, reject) => {
 			const didWork = this.consume(commandQueue);
 			if(!didWork) {resolve();}
@@ -18,7 +20,7 @@ class Worker {
 		const command = commandQueue.shift();
 		if(command) {
 			this.bot.say(command);
-			console.log(`sent command ${command}`);
+			console.log(`${this.bot.username}: sent command ${command}`);
 			setTimeout(() => { this.consume(commandQueue); }, 2000);
 		}
 		return !!command;
@@ -27,24 +29,53 @@ class Worker {
 
 export function start() {
 	dotenv.config();
+	
+	let chatbots = [];
 
-	const bot = new twitch({
-		username: process.env.USERNAME,
-		oauth: process.env.OAUTH,
-		channels: [process.env.CHANNEL]
-	});
-	
-	let commandQueueGlobal = [];
-	const commandString = fs.readFileSync(path.resolve(__dirname,COMMAND_SOURCE), "utf8");
-	commandQueueGlobal = commandString.split('\n');
-	
-	console.log('joining');
-	bot.on('join', () => {
-		const worker = new Worker();
-		worker.start(commandQueueGlobal, bot);
-	});
-	
-	bot.on('error', err => {
-		console.error(err);
+	if(process.env.ACCOUNTS) {
+		const accounts = JSON.parse(process.env.ACCOUNTS);
+		accounts.forEach(account => {
+			chatbots.push(new twitch({
+				username: account.username,
+				oauth: account.oauth,
+				channels: [process.env.CHANNEL]
+			}));
+		})
+	} else {
+		const bot = new twitch({
+			username: process.env.USERNAME,
+			oauth: process.env.OAUTH,
+			channels: [process.env.CHANNEL]
+		});
+		chatbots.push(bot);
+	}
+
+	let i = 1;
+	chatbots.forEach(bot => {
+		let commandQueueGlobal = [];
+		const commandString = fs.readFileSync(path.resolve(__dirname,`commands${i}.txt`), "utf8");
+		commandQueueGlobal = commandString.split('\n');
+		
+		console.log(`${bot.username} joining`);
+		bot.on('join', () => {
+			const worker = new Worker();
+			worker.name = bot.username;
+			console.log(`${bot.username} joined`);
+			setTimeout(() => {worker.start(commandQueueGlobal, bot);}, Math.random() * 5000);
+			
+		});
+		
+		bot.on('error', err => {
+			console.error(err);
+		})
+
+		bot.on('timeout', evt => {
+			console.log(evt);
+		})
+
+		bot.on('part', channel => {
+			console.log('part');
+		})
+		i++;
 	})
 }
--- pixelgen.ts
+++ pixelgen.ts
@@ -4,40 +4,74 @@ import * as _ from 'lodash'
 import * as path from 'path'
 import {COMMAND_SOURCE} from './bot';
 
-const IMG_URL = 'https://i.imgur.com/hwAlcbT.png';
-const ORIGIN_OFFSETS = {x:0, y:600};
-const BLACK_NO_OP_THRESHOLD = 10;
+const IMG_URL = 'https://i.imgur.com/rRECLEq.jpg';
+const ORIGIN_OFFSETS = {x:0, y:0};
+const BLACK_NO_OP_THRESHOLD = 35;
 
 console.log(`generating draw commands from source ${IMG_URL}`)
-pixels(IMG_URL, (err, pixData) => {
-	if(err) {
-		console.error(err);
-		return;
-	}
-	
-	let commands = [];
-	const width = pixData.shape[0];
-	const height = pixData.shape[1];
-	let lastColor;
-	for (let y = 0; y < height; y++) {
-		for (let x = 0; x < width; x++) {
-			const index = (y*width+x)*4
-			const color = pixData.data.slice(index, index + 4);
-			if(color[0] <= BLACK_NO_OP_THRESHOLD && color[1] <= BLACK_NO_OP_THRESHOLD && color[2] <= BLACK_NO_OP_THRESHOLD) {
-				//black skip it
-				continue
-			}
-			if(!lastColor || !_.isEqual(lastColor, color)) {
-				commands.push(`setrgb(${color[0]},${color[1]},${color[2]})`)
-				lastColor = color;
-			}
-			commands.push(`pp(${ORIGIN_OFFSETS.x+x},${ORIGIN_OFFSETS.y+y})`)
+
+pixels(`https://i.imgur.com/3K7DJxl.png`, (err, sourcePixData) => {
+	pixels(IMG_URL, (err, pixData) => {
+		if(err) {
+			console.error(err);
+			return;
 		}
-	}
-	console.log(commands);
-	let commandString = '';
-	commands.forEach(el => {
-		commandString += el + `\n`
-	})
-	fs.writeFileSync(path.resolve(__dirname,COMMAND_SOURCE), commandString);
-});
+		
+		let commands = [];
+		let groupedCommands = {};
+		let skips = 0;
+		const width = pixData.shape[0];
+		const height = pixData.shape[1];
+		let lastColor;
+		for (let y = 0; y < height; y++) {
+			for (let x = 0; x < width; x++) {
+				const index = (y*width+x)*4
+				const color = pixData.data.slice(index, index + 4);
+				const sourceColor = sourcePixData.data.slice(index, index + 4);
+				if(sourceColor[0] > 10 || sourceColor[1] > 10 || sourceColor[2] > 10) {
+					skips++;
+					continue;
+				}
+				if(color[0] <= BLACK_NO_OP_THRESHOLD && color[1] <= BLACK_NO_OP_THRESHOLD && color[2] <= BLACK_NO_OP_THRESHOLD) {
+					//black skip it
+					continue
+				}
+				color[0] = color[0] - color[0] % 2;
+				color[1] = color[1] - color[1] % 2;
+				color[2] = color[2] - color[2] % 2;
+				if(!lastColor || !_.isEqual(lastColor, color)) {
+					commands.push(`setrgb(${color[0]},${color[1]},${color[2]})`)
+					lastColor = color;
+				}
+				commands.push(`pp(${ORIGIN_OFFSETS.x+x},${ORIGIN_OFFSETS.y+y})`)
+				const colAr = [color[0],color[1],color[2]]
+				// console.log(`AO ${JSON.stringify(colAr)}`)
+				if(!groupedCommands[JSON.stringify(colAr)]) {
+					groupedCommands[JSON.stringify(colAr)] = [];
+				}
+				groupedCommands[JSON.stringify(colAr)].push(`pp(${ORIGIN_OFFSETS.x+x},${ORIGIN_OFFSETS.y+y})`);
+			}
+		}
+		// console.log(commands);
+		// console.log(skips);
+		let betterCommands = [];
+		let c = 0;
+		for (const key in groupedCommands) {
+			if (groupedCommands.hasOwnProperty(key)) {
+				const element = groupedCommands[key];
+				const color = JSON.parse(key);
+				// console.log(key);
+				betterCommands.push(`setrgb(${color[0]},${color[1]},${color[2]})`)
+				element.forEach(command => {
+					betterCommands.push(command);
+				})
+				c += element.length;
+			}
+		}
+		let commandString = '';
+		betterCommands.forEach(el => {
+			commandString += el + `\n`
+		})
+		fs.writeFileSync(path.resolve(__dirname,COMMAND_SOURCE), commandString);
+	});
+})
